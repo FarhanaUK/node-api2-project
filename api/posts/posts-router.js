b0cd30e9d38b40c5820fe22e17418d688fcf2b1e
Farhana Aktar <farhanaaktar@live.co.uk>
2024-11-04 14:55:54 +0000

    setup for enpoints

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -8,4 +8,23 @@ router.get('/', (req, res) => {
     res.json('foo')
 })
 
+router.get('/:id', (req, res) => {
+    res.json('foo')
+})
+
+router.post('/', (req, res) => {
+    res.json('foo')
+})
+
+router.delete('/:id', (req, res) => {
+    res.json('foo')
+})
+router.put('/:id', (req, res) => {
+    res.json('foo')
+})
+
+router.get('/:id:message', (req, res) => {
+    res.json('foo')
+})
+
 module.exports = router
