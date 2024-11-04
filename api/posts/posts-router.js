f0e29fcfb4702879afc135f414ed0ba10070b873
Farhana Aktar <farhanaaktar@live.co.uk>
2024-11-04 14:49:48 +0000

    parameter add the express Router

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,31 +1,11 @@
 const express = require('express')
-const Posts = require('./posts-model')
-const router = express.Router
+//const Posts = require('./posts-model')
+const router = express.Router()
 
 
 
 router.get('/', (req, res) => {
-    
-})
-
-router.get('/:id', (req, res) => {
-    
-})
-
-router.post('/', (req, res) => {
-    
-})
-
-router.put('/:id', (req, res) => {
-    
-})
-
-router.delete('/:id', (req, res) => {
-    
-})
-
-router.get('/:id/messages', (req, res) => {
-    
+    res.json('foo')
 })
 
 module.exports = router
