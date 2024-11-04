b9318744edeaf4ed1914c965e537e0aa1233fcfe
Farhana Aktar <farhanaaktar@live.co.uk>
2024-11-04 12:05:37 +0000

    index and server setup

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // require your server and launch it here
+const server = require('./api/server')
+
+const PORT = 5000
+
+
+
+server.listen(PORT, () => {
+    console.log('server starts on', PORT)
+})
