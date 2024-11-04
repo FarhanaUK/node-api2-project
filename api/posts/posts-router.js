64aa36526ae02b1132fd24521d13ffc8991d8ac8
Farhana Aktar <farhanaaktar@live.co.uk>
2024-11-04 14:42:23 +0000

    all 6 endpoints started

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -4,6 +4,28 @@ const router = express.Router
 
 
 
+router.get('/', (req, res) => {
+    
+})
 
+router.get('/:id', (req, res) => {
+    
+})
+
+router.post('/', (req, res) => {
+    
+})
+
+router.put('/:id', (req, res) => {
+    
+})
+
+router.delete('/:id', (req, res) => {
+    
+})
+
+router.get('/:id/messages', (req, res) => {
+    
+})
 
 module.exports = router
