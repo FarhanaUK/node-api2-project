5779628e87396d26d8223cf8aff2bf82a7feb157
Farhana Aktar <farhanaaktar@live.co.uk>
2024-11-04 15:20:08 +0000

    5 tests passed

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,19 +1,49 @@
 const express = require('express')
-//const Posts = require('./posts-model')
+const Post = require('./posts-model')
 const router = express.Router()
 
 
 
 router.get('/', (req, res) => {
-    res.json('foo')
+    Post.find()
+    .then(found => {
+        res.json(found)
+    })
+    .catch(err => {
+        res.status(500).json({
+            message: "The posts information could not be retrieved",
+            err: err.message,
+            stack: err.stack,
+        })
+    })
 })
 
 router.get('/:id', (req, res) => {
-    res.json('foo')
+     const id = req.params.id
+    Post.findById(id)
+   
+    .then(post => {
+        if(post){
+          res.json(post)  
+        }else{
+            res.status(404).json({
+                message: "The post with the specified ID does not exist"
+            })
+        }
+        
+    })
+    .catch(err => {
+        res.status(500).json({
+            message: "The post with the specified ID does not exist",
+            err: err.message,
+            stack: err.stack,
+        })
+    }
+    )
 })
 
 router.post('/', (req, res) => {
-    res.json('foo')
+    
 })
 
 router.delete('/:id', (req, res) => {
